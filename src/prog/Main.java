49197c88a15e7ba70b4a109f2d22bc59b027f078
eugenewongyj <eugenewongyj@gmail.com>
2022-12-02 16:18:28 +0800

    initial commit

---
+++ src/prog/Main.java
@@ -0,0 +1,40 @@
+package prog;
+
+import cart.Apple;
+import cart.Item;
+import cart.Orange;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Eugene: This section was just to create simple objects
+        // Item apple = new Item("appl", "Apple");
+        // Item orange = new Item("orange", "Orange");
+
+        // apple.setPrice(.5f);
+        // apple.setQuantity(10);
+
+        // orange.setPrice(.5f);
+        // orange.setQuantity(10);
+
+        // This section was to create inherited objects
+        Apple apple = new Apple();
+        apple.setType("washington");
+        Orange orange = new Orange();
+        System.out.printf("apple code: %s\n", apple.getCode());
+        System.out.printf("orange code: %s\n", orange.getCode());
+
+        Item item = apple;
+        System.out.printf("item code: %s\n", item.getCode());
+
+        Apple washington = (Apple) item;
+        System.out.printf("washington code: %s\n", washington.getCode());
+
+        // This will lead to error without check
+        System.out.printf("is item an Orange? %b\n", item instanceof Orange);
+        if (item instanceof Orange) {
+            orange = (Orange) item;
+        }
+    }
+    
+}
